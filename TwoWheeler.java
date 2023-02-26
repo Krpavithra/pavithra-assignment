8750a2c087f7177b1cfd4223109ca6e5f02e9fb3
Krpavithra <126324951+Krpavithra@users.noreply.github.com>
2023-02-26 16:57:58 +0530

    day1 assignment

---
+++ TwoWheeler.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class TwoWheeler
+{
+int noOfWheels=2;
+short  noOFMirrors=2;
+long chassisNumber=821767904;
+boolean isPunctured=true;
+String bikeName="Apache rr310";
+double runningKM=210000.83;
+public static void main(String[] args)
+{
+	TwoWheeler myBike= new TwoWheeler();
+	System.out.println(myBike.bikeName);
+	System.out.println("chassisNumber="+myBike.chassisNumber);
+	System.out.println("isPunctured="+myBike.isPunctured);
+	System.out.println("runningKM="+myBike.runningKM);
+	System.out.println("noOfWheels="+myBike.noOfWheels);
+	System.out.println("noOfMirrors="+myBike.noOFMirrors);
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+}
