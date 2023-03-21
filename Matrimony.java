fd5ed0cdb09da05b33ba207ae355a8c51776c4f8
Krpavithra <126324951+Krpavithra@users.noreply.github.com>
2023-03-21 09:01:14 +0530

    week 4 assignment on public sites

---
+++ Matrimony.java
@@ -0,0 +1,10 @@
+package week4.day2;
+
+public class Matrimony {
+public static void main(String[] args) {
+	EdgeDriver driver=new EdgeDriver();
+	driver.get("https://www.tamilmatrimony.in/");
+	driver.manage().window().maximixe();
+}
+
+}
