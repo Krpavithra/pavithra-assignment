a33c6f96a9efe3b428547b9c6ee8263d6147d916
Krpavithra <126324951+Krpavithra@users.noreply.github.com>
2023-03-21 11:18:44 +0530

    Add files via upload

---
+++ day2/Facebook.java
@@ -0,0 +1,12 @@
+package week4.day2;
+
+
+public class Facebook {
+	public static void main(String[] args) {
+		EdgeDriver driver=new EdgeDriver(); 
+		driver.get("https://www.facebook.com/login/");
+		driver.manage().window().maximixe();
+		driver.findElement(B)
+	}
+
+}
---
+++ day2/Matrimony1.java
@@ -0,0 +1,9 @@
+package week4.day2;
+import java.
+
+public class Matrimony1 {
+	public static void main(String[] args) {
+		EdgeDriver driver=new EdgeDriver();
+		driver.get("https://www.tamilmatrimony.in/");
+		driver.manage().window().maximixe();
+}
