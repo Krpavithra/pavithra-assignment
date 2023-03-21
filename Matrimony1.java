b6b5ae7159d40d330846fa6565ebf2b740f8c93a
Krpavithra <126324951+Krpavithra@users.noreply.github.com>
2023-03-21 09:10:20 +0530

    Delete Matrimony1.java

--- Matrimony1.java
+++
@@ -1,9 +0,0 @@
-package week4.day2;
-import java.
-
-public class Matrimony1 {
-	public static void main(String[] args) {
-		EdgeDriver driver=new EdgeDriver();
-		driver.get("https://www.tamilmatrimony.in/");
-		driver.manage().window().maximixe();
-}
